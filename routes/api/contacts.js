f33ebcb3b0679a04a31ded23620470b027b159be
Yurtov <123323348+Yurtov@users.noreply.github.com>
2023-11-12 15:22:27 +0200

    first push

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,4 +1,19 @@
+<<<<<<< Updated upstream
 const express = require('express')
+=======
+const express = require("express");
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
+  updateContactFavorite
+} = require("../../controllers/contacts");
+const { validateBody } = require("../../middlewars/validateBody");
+const isValidId = require("../../middlewars/isValidId");
+const { addSchema, updateFavoriteSchema } = require("../../models/contact");
+>>>>>>> Stashed changes
 
 const router = express.Router()
 
@@ -6,14 +21,19 @@ router.get('/', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
 
+<<<<<<< Updated upstream
 router.get('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
+=======
+router.get("/:contactId", isValidId, getContactById);
+>>>>>>> Stashed changes
 
 router.post('/', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
 
+<<<<<<< Updated upstream
 router.delete('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
@@ -21,5 +41,12 @@ router.delete('/:contactId', async (req, res, next) => {
 router.put('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
+=======
+router.delete("/:contactId", isValidId, removeContact);
+
+router.put("/:contactId", isValidId, validateBody(addSchema), updateContact);
+
+router.patch("/:contactId/favorite", isValidId, validateBody(updateFavoriteSchema), updateContactFavorite);
+>>>>>>> Stashed changes
 
 module.exports = router
